1ed1738e8ea7166d287096d1e828b4183f0916d2
shreya22 <shreya22@mit.edu>
2025-03-18 23:08:59 -0400

    Add legend

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -5,22 +5,39 @@
 	startAngle: 0,
 	endAngle: 2 * Math.PI
 });
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+	{ value: 1, label: "apples" },
+	{ value: 2, label: "oranges" },
+	{ value: 3, label: "mangos" },
+	{ value: 4, label: "pears" },
+	{ value: 5, label: "limes" },
+	{ value: 5, label: "cherries" }
+];
 
-let sliceGenerator = d3.pie();
+
+let sliceGenerator = d3.pie().value(d => d.value);
+
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 
 </script>
-
-<svg viewBox="-50 -50 100 100">
-{#each arcs as arc, index}
-  <path d={ arc } fill={ colors(index) } />
-{/each}
-
-</svg>
+<div class="container">
+    <svg viewBox="-50 -50 100 100">
+        {#each arcs as arc, index}
+            <path d={ arc } fill={ colors(index) } />
+        {/each}
+    </svg>
+	<ul class="legend">
+		{#each data as d, index}
+		    <li style="--color: { colors(index) }">
+			    <span class="swatch"></span>
+			    {d.label} <em>({d.value})</em>
+		    </li>
+	    {/each}
+	</ul>
+</div>
 
 <style>
     svg {
@@ -30,4 +47,40 @@ let colors = d3.scaleOrdinal(d3.schemeTableau10);
 	/* Do not clip shapes outside the viewBox */
 	overflow: visible;
 }
+  .container {
+    display: flex;
+
+    gap: 2rem; /* Space between pie chart and legend */
+  }
+
+
+  .swatch {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background-color: var(--color);
+    border-radius: 4px;
+    vertical-align: middle;
+  }
+
+  ul {
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(8em, 1fr));
+    gap: 1rem;
+    list-style: none;
+    padding: 1rem;                /* Space inside the border */
+    margin: 1.5rem 0;             /* Space outside the border */
+    border: 1px solid black;      /* Border to distinguish legend */
+    border-radius: 8px;           /* Optional: Rounded corners */
+    border-color: #f9f9f9;    /* Optional: Slight background color */
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+
 </style>
