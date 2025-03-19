72b90d8854e9665d02fe3e0986977f7e9f37cbb0
shreya22 <shreya22@mit.edu>
2025-03-19 14:55:19 -0400

    Add actual data

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,15 +1,24 @@
 <script>
+  import * as d3 from 'd3';
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
   import Pie from '$lib/Pie.svelte';
+  let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+
+  let pieData = rolledData.map(([year, count]) => {
+	return { value: count, label: year };
+});
+
+
+
 </script>
-  
+
 <svelte:head>
   <title>My Projects</title>
 </svelte:head>
 
 <h1> {projects.length} Projects</h1>
-<Pie />
+<Pie data={pieData} />
 <div class="projects">
     {#each projects as p}
         <Project data={p} />
