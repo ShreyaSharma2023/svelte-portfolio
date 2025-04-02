f7ee5c1d20554c491f91b03550924ca90e298e33
shreya22 <shreya22@mit.edu>
2025-04-02 16:15:18 -0400

    Update ./loc.csv

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -33,7 +33,7 @@ let data = [];
 let commits = [];
 let totalFiles = 0; // Variable to store total file count
 onMount(async () => {
-	data = await d3.csv("/loc.csv", row => ({
+	data = await d3.csv("./loc.csv", row => ({
 	...row,
 	line: Number(row.line), // or just +row.line
 	depth: Number(row.depth),
