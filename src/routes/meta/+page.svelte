085f8808614de42d4c46fddf81a33c0029e6787e
shreya22 <shreya22@mit.edu>
2025-04-02 16:25:54 -0400

    Add correct quotes

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+import { base } from '$app/paths';
 import Bar from '$lib/Bar.svelte';
 import * as d3 from "d3";
 import { onMount } from "svelte";
@@ -33,7 +34,7 @@ let data = [];
 let commits = [];
 let totalFiles = 0; // Variable to store total file count
 onMount(async () => {
-	data = await d3.csv("./loc.csv", row => ({
+	data = await d3.csv(`${base}/loc.csv`, row => ({
 	...row,
 	line: Number(row.line), // or just +row.line
 	depth: Number(row.depth),
