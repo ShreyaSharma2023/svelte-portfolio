4ac00ac15d3a7a299cb957c8f78ce45c9060b848
shreya22 <shreya22@mit.edu>
2025-04-02 09:41:56 -0400

    Add display summary stats

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,6 +1,59 @@
+<script>
+import * as d3 from "d3";
+import { onMount } from "svelte";
+let data = [];
+let commits = [];
+let totalFiles = 0; // Variable to store total file count
+onMount(async () => {
+	data = await d3.csv("/loc.csv", row => ({
+	...row,
+	line: Number(row.line), // or just +row.line
+	depth: Number(row.depth),
+	length: Number(row.length),
+	date: new Date(row.date + "T00:00" + row.timezone),
+	datetime: new Date(row.datetime),
+}));
+// Count unique files
+const fileSet = new Set(data.map(d => d.file)); // Assuming there's a 'file' column
+totalFiles = fileSet.size;
+commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+	let first = lines[0];
+	let {author, date, time, timezone, datetime} = first;
+	let ret = {
+		id: commit,
+		url: "https://github.com/vis-society/lab-7/commit/" + commit,
+		author, date, time, timezone, datetime,
+		hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+		totalLines: lines.length
+	};
+	// Like ret.lines = lines
+	// but non-enumerable so it doesn’t show up in JSON.stringify
+	Object.defineProperty(ret, "lines", {
+		value: lines,
+		configurable: true,
+		writable: true,
+		enumerable: false,
+	});
+	return ret;
+});
+console.log(commits)
+});
+</script>
+
 <svelte:head>
   <title>Meta</title>
 </svelte:head>
 
 <h1>Meta</h1>
-<p>This is the meta page from Lab 6</p>
+<h2>Summary</h2>
+<dl class="stats">
+	<dt>Total <abbr title="Lines of code">LOC</abbr></dt>
+	<dd>{data.length}</dd>
+  <dt>Total <abbr title="Commits">Commits</abbr></dt>
+  <dd>{commits.length}</dd>
+  <dt>Total <abbr title="Files">Files</abbr></dt>
+	<dd>{totalFiles}</dd>
+</dl>
+
+
+
